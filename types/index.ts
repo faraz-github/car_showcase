13da49355f09e2525457cffe6c733544e380afcb
farazahmad <farazahmad.email@gmail.com>
2023-07-08 10:54:44 +0530

    Create cars catalogue card

--- types/index.ts
+++ types/index.ts
@@ -1,17 +1,41 @@
 import { MouseEventHandler } from "react";
 
+// Component Props Types
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>
-    btnType?: "button" | "submit";
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface CustomFilterProps {
-    title?: string;
+  title?: string;
 }
 
 export interface SearchManufacturerProps {
-    manufacturer: string;
-    setManufacturer: (manufacturer: string) =>  void;
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarCardProps {
+  car: CarProps;
+}
+
+// API Response Types
+export interface CarProps {
+  city_mpg: number;
+  class: string; // TODO class is a keyword
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
